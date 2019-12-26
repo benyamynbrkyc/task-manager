a3fb6f8622010f95f249236c12330706ef516a52
benyamynbrkyc <benjamin.brkic@gmail.com>
2019-12-26 16:48:39 +0100

    Added authorization: login, logout, logout all, sign up, tokens

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const task_router = require('./routes/tasks');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method == 'GET') {
+//     res.send('GET requests are disabled');
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send('Site under maintenance');
+// });
+
 app.use(express.json());
 app.use(user_router);
 app.use(task_router);
