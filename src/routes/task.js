19dc76e59af615070e3d815b8ece406551038093
benyamynbrkyc <benjamin.brkic@gmail.com>
2019-12-27 22:46:17 +0100

    Added sorting, pagination, timestamps

--- src/routes/task.js
+++ src/routes/task.js
@@ -20,15 +20,39 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sort_by=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sort_by) {
+    const parts = req.query.sort_by.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    // await req.user.populate('tasks').execPopulate();
-    // res.send(req.user.tasks)
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
+
     if (!tasks) {
       return res.status(404).send();
     }
-    res.status(200).send(tasks);
   } catch (error) {
     res.status(400).send(error);
   }
